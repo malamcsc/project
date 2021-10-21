f0e623275193d3ee405fbf31d8dc2235cd037830
malamcsc <55490652+malamcsc@users.noreply.github.com>
2021-10-21 22:03:10 +0530

    Update prod.py

--- prod.py
+++ prod.py
@@ -11,7 +11,7 @@ DB_UNAME = 'malamcsc'       # PostgreSQL user name
 DB_PWD = 'Patna@123'           # PostgreSQL user password
 
 
-pgsql_connection_string = "postgres://"+DB_UNAME+"%40"+DB_PGNAME+":"+DB_PWD+"@"+DB_PGNAME+".postgres.database.azure.com"+":"+DB_PORT+"/"
+pgsql_connection_string = "postgresql://"+DB_UNAME+"%40"+DB_PGNAME+":"+DB_PWD+"@"+DB_PGNAME+".postgres.database.azure.com"+":"+DB_PORT+"/"
 
 #pgsql_connection_string = "postgresql+psycopg2://"+DB_UNAME+":"+DB_PWD+"@"+DB_PGNAME+":"+DB_PORT+"/"
                                 
